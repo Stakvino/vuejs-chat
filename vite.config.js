b2cc3473768b15ed04aa094af9ff91343108d45b
Stakvino <stakvino@gmail.com>
2025-06-26 20:06:35 -0400

    changing vite file for images path on build

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
@@ -23,3 +23,32 @@ export default defineConfig({
         },
     },
 })
+*/
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: "0.0.0.0",
+        },
+        port: 3000,
+        host: true,
+    },
+    plugins: [
+        vue({
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
+
