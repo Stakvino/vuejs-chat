1afc68259002ed06534e117ae1996d6118902feb
Stakvino <stakvino@gmail.com>
2025-07-13 14:25:28 -0400

    remove domain name from web routes

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['domain' => 'vuejschat.oussama-cheriguene.com'], function () {
+// Route::group(['domain' => 'vuejschat.oussama-cheriguene.com'], function () {
 
     Route::get('/{all}', function () {
         return view('homepage');
     })
     ->where(['all' => '^(?!api|verify-email).*$']);
 
-});
+// });
 
 
 require __DIR__.'/auth.php';
