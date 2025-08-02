6c309404f852ef58daedc8602952cf05873e506d
Stakvino <stakvino@gmail.com>
2025-08-01 23:58:40 -0400

    trying axios.defaults.headers.common

--- resources/js/views/LoginView.vue
+++ resources/js/views/LoginView.vue
@@ -12,7 +12,7 @@
 
     axios.defaults.headers.common = {
         'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     };
     axios.defaults.withCredentials = true;
 
