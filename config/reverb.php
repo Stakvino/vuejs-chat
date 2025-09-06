4a6030e1503765a8dc4d3b759855e95b21bdce63
Stakvino <stakvino@gmail.com>
2025-09-06 11:22:53 -0400

    reverb config change

--- config/reverb.php
+++ config/reverb.php
@@ -34,7 +34,10 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert' => '/etc/nginx/ssl/fullchain.pem',
+                    'local_pk' => '/etc/nginx/ssl/privkey.pem',
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
